8f8ec1bf017e76719c347384473fdd48a773e823
Adrian Catalan <adrianblade6@gmail.com>
2023-08-26 01:47:41 +0200

    fix: script.docker

--- docker/CreateJobWithSharedLibrary.groovy
+++ docker/CreateJobWithSharedLibrary.groovy
@@ -24,7 +24,7 @@ quickstarterPipeline(
       sh "echo hi!"
   }
 
-  compileProject(context, [command: "./gradlew build", configFileId: null])
+  //compileProject(context, [command: "./gradlew build", configFileId: null])
 
 }
 """, true)
